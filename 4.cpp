f3afb629f4074263cd70282b839e426f7531f1c5
dbmamanir <dbmamanir@unjbg.edu.pe>
2024-08-13 12:16:56 -0500

    suma de la diagonal secundaria

---
+++ 4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<<"Ingrese el valor de la matriz:";
+    cin>>n;
+    int matriz[n][n];
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            cout<<"ingrese el valor:";
+            cin>>matriz[i][j];
+        }
+    }
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            cout<<matriz[i][j];
+        }
+        cout<<endl;
+    }
+    int s=0;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            if (i+j==n-1){
+                s=s+matriz[i][j];
+            }
+        }
+    }
+    cout<<"la sumatoria de la diagonal secundaria es:"<<s<<endl;
+    return 0;
+}
